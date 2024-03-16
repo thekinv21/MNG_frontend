de525831c338326b0a83aab38d5dd3363470ce8e
Vadim Kiniabaev <thekinv21@gmail.com>
2024-03-16 21:51:39 +0300

    FEAT:[TN-2][VADIM] add PATH alias for react-native tools for project

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,22 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel"],
-  };
-};
+module.exports = function (api) {
+	api.cache(true)
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: [
+			'nativewind/babel',
+
+			[
+				'module-resolver',
+				{
+					root: ['./src'],
+					extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+					alias: {
+						'@/': './src',
+						'@/components': './src/components',
+						'@/screens': './src/screens'
+					}
+				}
+			]
+		]
+	}
+}

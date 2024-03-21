aa95bac3bcef51d50a9d1c76364927867cd34230
Vadim Kiniabaev <thekinv21@gmail.com>
2024-03-22 02:05:46 +0300

    FEAT:[TN-2][VADIM] create login page with crud operations

---
+++ src/screens/auth/login/index.tsx
@@ -0,0 +1,76 @@
+import { ImagesAssets } from 'assets/imageAsset'
+import { FC } from 'react'
+import {
+	Image,
+	Pressable,
+	Text,
+	TextInput,
+	TouchableHighlight,
+	View
+} from 'react-native'
+
+import { useLogin } from './useLogin'
+
+const Login: FC = () => {
+	const { handleRegister, ...rest } = useLogin()
+
+	return (
+		<View className='flex-1  items-center justify-center'>
+			<View className='flex flex-col items-center justify-center'>
+				<Image className='h-80 w-80' source={ImagesAssets.loginLogo} />
+			</View>
+
+			<View className='w-full px-8'>
+				<TextInput
+					value={rest.email}
+					onChangeText={text => rest.setEmail(text)}
+					showSoftInputOnFocus={false}
+					placeholder='E-posta giriniz..'
+					autoCapitalize='none'
+					className={
+						'mt-3 w-full rounded-lg bg-gray-200 p-2.5 transition-all duration-200 ease-in-out'
+					}
+				/>
+
+				<TextInput
+					value={rest.password}
+					onChangeText={text => rest.setPassword(text)}
+					showSoftInputOnFocus={false}
+					placeholder='Şifre giriniz...'
+					autoCapitalize='none'
+					secureTextEntry={true}
+					className={
+						'mt-3 w-full rounded-lg bg-gray-200 p-2.5 transition-all duration-200 ease-in-out'
+					}
+				/>
+
+				<TouchableHighlight
+					onPress={() => {
+						rest.handleLogin()
+					}}
+					underlayColor={'none'}
+					className='mt-5 w-full rounded-lg bg-green-700 p-2.5'
+				>
+					<Text className=' text-center text-sm font-semibold uppercase text-gray-50'>
+						Giriş Yap
+					</Text>
+				</TouchableHighlight>
+
+				<View className='my-5 flex flex-row items-center'>
+					<View className='h-[1px] flex-1 bg-gray-400' />
+					<Text className='w-16 text-center'>Veya</Text>
+					<View className='h-[1px] flex-1 bg-gray-400' />
+				</View>
+
+				<View className='flex flex-row items-center justify-center'>
+					<Text className='text-gray-500'>Hesabınız yok mu? </Text>
+					<Pressable onPress={handleRegister}>
+						<Text className='ml-1 text-green-700'>Kayıt Ol</Text>
+					</Pressable>
+				</View>
+			</View>
+		</View>
+	)
+}
+
+export default Login
---
+++ src/screens/auth/login/useLogin.ts
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+import { ILogin } from '@/types/auth.types'
+
+import { useRoute } from '@/hooks/useRoute'
+
+import { AuthService } from '@/services/auth.service'
+
+import { saveUserToStorage } from '@/helpers/auth.helper'
+import { ROUTES } from '@/navigation/routes'
+
+export const useLogin = () => {
+	const { route } = useRoute()
+
+	const [email, setEmail] = useState<string>('')
+	const [password, setPassword] = useState<string>('')
+
+	const handleLogin = async () => {
+		const payload: ILogin = {
+			email: email,
+			password: password
+		}
+
+		try {
+			const response = await AuthService.login(payload)
+
+			if (response.status >= 200 && response.status < 300) {
+				saveUserToStorage(response.data)
+				route.navigate(ROUTES.MAIN as never)
+			}
+		} catch (error) {
+			console.log('error', error)
+		}
+	}
+
+	const handleRegister = () => {
+		route.navigate(ROUTES.REGISTER as never)
+	}
+
+	return {
+		email,
+		password,
+		setEmail,
+		setPassword,
+		handleRegister,
+		handleLogin
+	}
+}
